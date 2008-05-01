3e652a640894f775e9cb8fd4535b6d3ef84d7af8
xen <xen@4397e4d0-6c2f-0410-8fa1-b9089db04313>
2008-05-01 05:55:55 +0000

    * Add support to the parser adder for adding without grouping and for   excluding options.  Eneded up being unused, but might as well leave   it.
    git-svn-id: svn+ssh://rpatterson.net/var/local/repos/main/rpatterson.mailsync/trunk@50 4397e4d0-6c2f-0410-8fa1-b9089db04313

--- rpatterson/mailsync/parse.py
+++ rpatterson/mailsync/parse.py
@@ -1,8 +1,12 @@
-def add_options(parser, other, title):
+def add_options(parser, other, title=None, exclude=()):
     parser.set_defaults(**other.defaults)
-    group = parser.add_option_group(title, other.description)
+    if title:
+        group = parser.add_option_group(title, other.description)
+    else:
+        group = parser
     for option in other.option_list:
-        if not parser.has_option(option.get_opt_string()):
+        if not (option.get_opt_string() in exclude or
+                parser.has_option(option.get_opt_string())):
             group.add_option(option)
     for other_group in getattr(other, 'option_groups', ()):
         add_options(parser, other_group, other_group.title)
